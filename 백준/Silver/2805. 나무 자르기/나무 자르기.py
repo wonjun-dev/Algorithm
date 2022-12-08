6a6c98fc8d1fb6731954ffffe4869bcf1e9634b1
Wonjun <50730434+wonjun-dev@users.noreply.github.com>
2022-12-08 10:27:01 +0900

    [Silver II] Title: 나무 자르기, Time: 376 ms, Memory: 117816 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,28 +1,19 @@
 import sys
+from collections import Counter
+n,m = map(int, sys.stdin.readline().split())
+t = Counter(map(int, sys.stdin.read().split()))
 
-N, M = map(int, sys.stdin.readline().strip().split())
-tree = sorted(list(map(int, sys.stdin.readline().strip().split())))
+s = 1
+e = max(t.items())[0]
 
-s = 0
-e = tree[-1]
-h = (s + e) // 2
+while s<=e:
+    mid = (s+e)//2
+    tot = sum((h-mid)*i for h,i in t.items() if h>mid)
 
-while True:
-    cut = 0
-    for t in tree:
-        if t > h:
-            cut += t - h 
-    
-    if cut > M:
-        s = h
-        h = (s + e) // 2
-    elif cut < M:
-        e = h
-        h = (s + e) // 2
-    else:
-        break
-    
-    if h == e or h == s:
-        break
+    if tot >= m:
+        s = mid+1
+    elif tot <m:
+        # ans = mid
+        e = mid-1
 
-print(h)
+print(e)
