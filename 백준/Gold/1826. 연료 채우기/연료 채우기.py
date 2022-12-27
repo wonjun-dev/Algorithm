67f7f2aff15207dfebd7769506ab022af13bb5e0
Wonjun <50730434+wonjun-dev@users.noreply.github.com>
2022-12-27 10:42:34 +0900

    [Gold II] Title: 연료 채우기, Time: 68 ms, Memory: 33908 KB -BaekjoonHub

--- 백준/Gold/1826. 연료 채우기/연료 채우기.py
+++ 백준/Gold/1826. 연료 채우기/연료 채우기.py
@@ -1,32 +1,24 @@
 import sys
-import heapq
+from heapq import heapify, heappush, heappop
 
-n = int(sys.stdin.readline())
-ab = []
-[heapq.heappush(ab, list(map(int, sys.stdin.readline().split()))) for _ in range(n)]
+N = int(sys.stdin.readline())
+station = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+heapify(station)
 l, p = map(int, sys.stdin.readline().split())
-cnt = 0
-heap = []
 
-# 현재 있는 연료로 마을까지 갈수 있을 때까지 반복
+ans = 0
+tmp = []
 while p < l:
+    while station and station[0][0] <= p:
+        d, f = heappop(station)
+        heappush(tmp, (-f, d))
+    
+    if tmp:
+        f, d = heappop(tmp)
+        p += -f
+        ans += 1
 
-    # 앞으로 주유소가 있고 다음 주유소를 현재 연료로 갈 수 있으면 
-    # 현재 연료로 갈 수 있는 모든 주유소를 확인
-    while ab and ab[0][0] <= p:
-        # 주유소의 연료양 기준으로 최대힙을 만들어 리스트에 푸시한다.
-        a, b = heapq.heappop(ab)
-        heapq.heappush(heap, [-b, a])
-
-    # 현재 연료로 다음 주유소까지 갈 수 없으므로 -1 출력
-    if not heap:
-        cnt = -1
+    else:
+        ans = -1
         break
-
-    # 최대힙을 팝한다.(연료를 제일 많이 충전해주는 주유소)
-    b, a = heapq.heappop(heap)
-    # 연료를 충전한다.
-    p += -b
-    cnt += 1
-
-print(cnt)
+print(ans) 
